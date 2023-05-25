5bcbeb35d672c0bef816a2a722514da162ee059d
AFOEK <felix.montalfu@gmail.com>
2023-05-25 21:39:05 +0700

    fixing error when getting user name, and fixing time format

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import telebot
 from dotenv import load_dotenv 
 import connector as db
 import inject as qry
+import datetime
+import tzlocal
 load_dotenv()
 
 token = ''.join(os.environ.get("BOT_TOKEN"))
@@ -12,15 +14,18 @@ print("database succesfully connected")
 bot = telebot.TeleBot(token)
 print("bot succesfully connected")
 
+local_timezone = tzlocal.get_localzone()
+
 @bot.message_handler(commands=['in'])
 def signin(message):
     user_id_chat = message.from_user.id
-    user_name_chat = message.from_user.name
+    user_name_chat = message.from_user.username
+    user_real_name = message.from_user.first_name + ' ' + message.from_user.last_name
     message_id = message.message_id
     chat_time = message.date
     try:
-        qry.sign_in(id_chat=user_id_chat, mgs_id=message_id, chat_tm=chat_time)
-        bot.reply_to(message, f"{user_name_chat} in at {chat_time}. Sign in succesfully")
+        # qry.sign_in(id_chat=user_id_chat, mgs_id=message_id, chat_tm=chat_time)
+        bot.reply_to(message, f"{user_real_name} sign in at {datetime.datetime.fromtimestamp(chat_time, local_timezone)}. Sign in succesfully")
     except:
         bot.reply_to(message, "Failed to sign in !")
         print("failed to insert to database !")
@@ -28,12 +33,13 @@ def signin(message):
 @bot.message_handler(commands=['out'])
 def signin(message):
     user_id_chat = message.from_user.id
-    user_name_chat = message.from_user.name
+    user_name_chat = message.from_user.username
+    user_real_name = message.from_user.first_name + ' ' + message.from_user.last_name
     message_id = message.message_id
     chat_time = message.date
     try:
-        qry.sign_out(id_chat=user_id_chat, mgs_id=message_id, chat_tm=chat_time)
-        bot.reply_to(message, f"{user_name_chat} out at {chat_time}. Sign out succesfully")
+        # qry.sign_out(id_chat=user_id_chat, mgs_id=message_id, chat_tm=chat_time)
+        bot.reply_to(message, f"{user_real_name} out at {datetime.datetime.fromtimestamp(chat_time, local_timezone)}. Sign out succesfully")
     except:
         bot.reply_to(message, "Failed to sign in !")
         print("failed to insert to database !")
@@ -41,7 +47,10 @@ def signin(message):
 @bot.message_handler(commands=['init'])
 def init(message):
     user_id = message.from_user.id
-    member = message.chat.get_member(user_id)
+    chat_id = message.chat.id
+    member = bot.get_chat_member(user_id=user_id, chat_id=chat_id)
+    bot.reply_to(message, f"{member}")
+    print (member)
 
 if __name__ == "__main__":
     bot.infinity_polling()
