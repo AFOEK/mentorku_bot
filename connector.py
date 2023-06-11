2ad26d73c908cda0616cc70eddf28fbee241f568
AFOEK <felix.montalfu@gmail.com>
2023-06-11 13:18:34 +0700

    Fixing database connection

--- connector.py
+++ connector.py
@@ -4,46 +4,33 @@ import os
 load_dotenv()
 
 def db_connect():
-    # try:
-    #     print("Connecting from local network")
-    #     DB_ADD = os.environ.get("DB_ADD")
-    #     DB_USER = os.environ.get("DB_USER")
-    #     DB_PASSWORD = os.environ.get("DB_PASSWORD")
-    #     DB_NAME = os.environ.get("DB_NAME")
+    try:
+        print("Connecting from local network")
+        DB_ADD = os.environ.get("DB_ADD")
+        DB_USER = os.environ.get("DB_USER")
+        DB_PASSWORD = os.environ.get("DB_PASSWORD")
+        DB_NAME = os.environ.get("DB_NAME")
 
-    #     db = mysql.connector.connect(
-    #         host = ''.join(DB_ADD),
-    #         user = ''.join(DB_USER),
-    #         password = ''.join(DB_PASSWORD),
-    #         database = ''.join(DB_NAME)
-    #     )
-    # except:
-    #     print("Failed to connect from local network.\nConnecting using tunnel")
-    #     DB_ADD_TUNNEL = os.environ.get("DB_ADD_TUNNEL")
-    #     DB_USER = os.environ.get("DB_USER")
-    #     DB_PASSWORD = os.environ.get("DB_PASSWORD")
-    #     DB_NAME = os.environ.get("DB_NAME")
-    #     DB_PORT_TUNNEL = os.environ.get("DB_PORT_TUNNEL")
+        db = mysql.connector.connect(
+            host = ''.join(DB_ADD),
+            user = ''.join(DB_USER),
+            password = ''.join(DB_PASSWORD),
+            database = ''.join(DB_NAME)
+        )
+    except:
+        print("Failed to connect from local network.\nConnecting using tunnel")
+        DB_ADD_TUNNEL = os.environ.get("DB_ADD_TUNNEL")
+        DB_USER = os.environ.get("DB_USER")
+        DB_PASSWORD = os.environ.get("DB_PASSWORD")
+        DB_NAME = os.environ.get("DB_NAME")
+        DB_PORT_TUNNEL = os.environ.get("DB_PORT_TUNNEL")
 
-    #     db = mysql.connector.connect(
-    #         host = ''.join(DB_ADD_TUNNEL),
-    #         user = ''.join(DB_USER),
-    #         password = ''.join(DB_PASSWORD),
-    #         database = ''.join(DB_NAME),
-    #         port = ''.join(DB_PORT_TUNNEL)
-    #     )
-    
-    DB_ADD_TUNNEL = os.environ.get("DB_ADD_TUNNEL")
-    DB_USER = os.environ.get("DB_USER")
-    DB_PASSWORD = os.environ.get("DB_PASSWORD")
-    DB_NAME = os.environ.get("DB_NAME")
-    DB_PORT_TUNNEL = os.environ.get("DB_PORT_TUNNEL")
-    db = mysql.connector.connect(
-        host = ''.join(DB_ADD_TUNNEL),
-        user = ''.join(DB_USER),
-        password = ''.join(DB_PASSWORD),
-        database = ''.join(DB_NAME),
-        port = ''.join(DB_PORT_TUNNEL)
-    ) 
+        db = mysql.connector.connect(
+            host = ''.join(DB_ADD_TUNNEL),
+            user = ''.join(DB_USER),
+            password = ''.join(DB_PASSWORD),
+            database = ''.join(DB_NAME),
+            port = ''.join(DB_PORT_TUNNEL)
+        )
 
     return db
